e78000280ccee215fe997f8c4b0fbb0957cd81b1
raffaele-catalano <raf.catalano@gmail.com>
2023-05-15 15:29:29 +0200

    setup landing-page

---
+++ landing-page.php
@@ -0,0 +1,47 @@
+<?php
+
+
+$paragraph      = $_GET['paragraph'];
+$wordToCensure  = $_GET['words'];
+
+
+// 
+var_dump([$paragraph, $wordToCensure]);
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- font-awesome -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css' integrity='sha512-Z0kTB03S7BU+JFU0nw9mjSBcRnZm2Bvm0tzOX9/OuOuz01XQfOpa0w/N9u6Jf2f1OAdegdIPWZ9nIZZ+keEvBw==' crossorigin='anonymous'/>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="assets/img/badwords_fav.png" type="png">
+    <!-- Bootstrap -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css' integrity='sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==' crossorigin='anonymous'/>
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Badwords</title>
+</head>
+
+<body>
+
+    <div class="container d-flex justify-content-center align-items-center flex-column my-3 p-3">
+
+    <h1 class="text-center mb-4">Here's your Result</h1>
+
+    
+
+    </div>
+
+</body>
+
+</html>
